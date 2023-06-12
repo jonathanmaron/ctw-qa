291792e4e622b5ba9155f849dbaf84cf528b5c23
Jonathan Maron <jonathan.maron@theimagingsource.com>
2023-06-12 11:04:56 +0200

    Refactoring

--- src/Rector/Config/RectorConfig/DefaultSkip.php
+++ src/Rector/Config/RectorConfig/DefaultSkip.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace Ctw\Qa\Rector\Config\RectorConfig;
 
+use Rector\Naming\Rector\ClassMethod\RenameVariableToMatchNewTypeRector;
 use Rector\Naming\Rector\Assign\RenameVariableToMatchMethodCallReturnTypeRector;
 use Rector\Naming\Rector\ClassMethod\RenameParamToMatchTypeRector;
 use Rector\Php81\Rector\FuncCall\NullToStrictStringFuncCallArgRector;
@@ -19,6 +20,7 @@ class DefaultSkip
             NullToStrictStringFuncCallArgRector::class,
             RenameParamToMatchTypeRector::class,
             RenameVariableToMatchMethodCallReturnTypeRector::class,
+            RenameVariableToMatchNewTypeRector::class,
         ];
     }
 }
