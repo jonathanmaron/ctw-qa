b1ca4de00f34b71741217ffdecef7f357ac60d67
Jonathan Maron <jonathan.maron@theimagingsource.com>
2024-06-14 09:19:26 +0200

    Set LevelSetList::UP_TO_PHP_83

--- src/Rector/Config/RectorConfig/DefaultSets.php
+++ src/Rector/Config/RectorConfig/DefaultSets.php
@@ -12,7 +12,7 @@ class DefaultSets
     public function __invoke(): array
     {
         return [
-            LevelSetList::UP_TO_PHP_81,
+            LevelSetList::UP_TO_PHP_83,
             PHPUnitSetList::PHPUNIT_100,
             SetList::CODE_QUALITY,
             SetList::CODING_STYLE,
